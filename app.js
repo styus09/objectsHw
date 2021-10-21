d8d464e2cf6f894bac8b61242c90725d1e2754d9
Sean <styus09@gmail.com>
2021-10-21 10:41:29 -0400

    objectsHw

---
+++ app.js
@@ -0,0 +1,46 @@
+function addTwo(a, b){
+    console.log(a + b)
+}
+addTwo(9, 27)
+
+function addThree(a, b, c) {
+    console.log(a + b + c)
+}
+addThree(9, 27, 33)
+
+function addFour(a, b, c, d) {
+    console.log(a + b + c + d)
+}
+addFour(9, 27, 33, 45)
+
+
+var dog = {
+    "name": "Shabba",
+    "legs": 4,
+    "tails": 2,
+    "enemies": ["Vacuum", "Cats"]
+  };
+  dog.trick = "roll over";
+  dog["snack"] = "Bone";
+  console.log(dog.enemies)
+console.log["tails"];
+
+  var bird = {
+    "type": "Cardinal",
+    "legs": 2,
+    "name": "Nix",
+  };
+  bird.color = "Red";
+  bird["sound"] = "Tweet";
+  console.log(bird.name)
+console.log["type"];
+
+  var car = {
+    "make": "Kia",
+    "model": "Optima",
+    "year": "2015",
+};
+car.wheels = 4;
+car["color"] = "white";
+console.log(car.model)
+console.log["year"];
